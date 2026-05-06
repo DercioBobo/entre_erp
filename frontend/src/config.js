f1e483d8a6cb84ca5ca69d904986fac6c1251bf0
Dércio Bobo <38473260+DercioBobo@users.noreply.github.com>
2026-05-06 16:30:25 +0200

    Fix on /support

--- frontend/src/config.js
+++ frontend/src/config.js
@@ -1,6 +1,6 @@
 export const portalCtx = {
-    customer:      window.__portal_ctx__?.customer      ?? "",
-    customerLabel: window.__portal_ctx__?.customerLabel ?? "",
-    user:          window.__portal_ctx__?.user          ?? "",
-    userFullname:  window.__portal_ctx__?.userFullname  ?? "",
+    customer:      window.portalCtx?.customer      ?? "",
+    customerLabel: window.portalCtx?.customerLabel ?? "",
+    user:          window.portalCtx?.user          ?? "",
+    userFullname:  window.portalCtx?.userFullname  ?? "",
 };
